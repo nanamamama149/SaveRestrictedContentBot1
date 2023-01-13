0736805e1a7cbbcbb8eb519ee69712cfa460977d
nanamamama149 <110466664+nanamamama149@users.noreply.github.com>
2023-01-13 20:11:19 +0545

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+API_ID = config("18601774", default=None, cast=int)
+API_HASH = config("218ada48a21b6a605f3123022d85974a", default=None)
+BOT_TOKEN = config("5785290597:AAFytyekZj9s93cjuaKDyFwo33PGQYfbuRA", default=None)
+SESSION = config("BQEb1y4AlWF5VcsmhfqUb7S3wwD5d5ZyQk4NAX6KUj_wsoK2gpkbvxNLXlMV89UYZFwDZrpZgMIQ4RuMFO-aSkey97--t3PQIZIhVawF1NsEXEfJv9VLjWRZiZHWQ1JUouliPkerEqKspRI0URAtz2-lSDq2mI1qdoIDv0paEwX2Zxq2GQLY9R8gMbw8qmN_oxBBQjcwXS-OlDl4Nh7XECTUk010FWBGQbZb5Bx7SDqqoZyaIwzujcFPaYyps0zw0fZltlzxgayLQbbfaXAmv84eoW-ubBTEKRSUuQD5hwOc_VqOHXY2iAVzZzUmcnJMuxyM4sYNcz1qI3aruzAmdfRcfmRJrQAAAABGexBPAA", default=None)
+FORCESUB = config("https://t.me/+4zuY__2ON344ODFh", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
